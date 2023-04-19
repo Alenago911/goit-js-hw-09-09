455f883663596f1061e27831ba2f8529b2ada300
Alenago911 <alena.chernyavka@gmail.com>
2023-04-19 16:18:33 +0300

    1 color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+let btnIsActive = (startBtn.disabled = false);
+let btnIsDisable = (stopBtn.disabled = true);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function onStartClick() {
+  if (btnIsActive) {
+    return;
+  }
+
+  intervalId = setInterval(callBackSetInterval, INTERVAL_DELAY);
+  btnIsActive = startBtn.disabled = true;
+  btnIsDisable = stopBtn.disabled = false;
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  btnIsActive = startBtn.disabled = false;
+  btnIsDisable = stopBtn.disabled = true;
+}
+
+function callBackSetInterval() {
+  return (body.style.backgroundColor = getRandomHexColor());
+}
